4ae0e734558c48e9ab5ab09efc5afff8f38272e1
Petar <petar.markovic.4977@metropolitan.ac.rs>
2025-03-19 21:43:29 +0100

    Service dialog
    Changed the button to be the choose service dialog

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -4,78 +4,67 @@ import React, {useCallback, useEffect, useState} from 'react';
 import Link from "next/link";
 import Image from "next/image";
 import ScheduleACallButton from "@/app/components/ScheduleACallButton";
-import {
-    Sheet,
-    SheetContent,
-    SheetDescription,
-    SheetFooter,
-    SheetHeader,
-    SheetTitle,
-    SheetTrigger
-} from "@/components/ui/sheet";
+import {Sheet, SheetContent, SheetTitle, SheetTrigger} from "@/components/ui/sheet";
 import {Menu} from "lucide-react";
 import ChooseServiceDialog from "@/app/components/choose-service-dialog/ChooseServiceDialog";
 
 export default function Navbar() {
 
-    const [scrollYPage, setScrollYPage] = useState(0);
+	const [scrollYPage, setScrollYPage] = useState(0);
 
-    const onScroll = useCallback(() => {
-        const {scrollY} = window;
-        setScrollYPage(scrollY);
-    }, []);
+	const onScroll = useCallback(() => {
+		const {scrollY} = window;
+		setScrollYPage(scrollY);
+	}, []);
 
-    useEffect(() => {
-        window.addEventListener("scroll", onScroll);
-        return () => {
-            window.removeEventListener("scroll", onScroll);
-        }
-    }, []);
+	useEffect(() => {
+		window.addEventListener("scroll", onScroll);
+		return () => {
+			window.removeEventListener("scroll", onScroll);
+		}
+	}, []);
 
-    return (
-        <nav
-            className="w-full flex items-center justify-between py-4 top-4 z-50 sticky bg-[#FFFFFF] border-1 rounded-xl px-4">
-            <div className="lg:logo-wrapper w-7/12">
-                <Link href={"/"}>
-                    <Image width={250} height={250} className="object-fit w-3/12 max-[1025px]:w-6/12" src="logo.png"
-                           alt="Webique Logo"/>
-                </Link>
-            </div>
+	return (
+		<nav
+			className="w-full flex items-center justify-between py-4 top-4 z-50 sticky bg-[#FFFFFF] border-1 rounded-xl px-4">
+			<div className="lg:logo-wrapper w-7/12">
+				<Link href={"/"}>
+					<Image width={250} height={250} className="object-fit w-3/12 max-[1025px]:w-6/12" src="logo.png"
+								 alt="Webique Logo"/>
+				</Link>
+			</div>
 
-            <div className="lg:inline-flex gap-8 items-center max-[1025px]:hidden">
-                <Link href={"/#services"} className="inline-flex items-center gap-1">
-                    Services
-                </Link>
-                <Link href={"/"}>Projects</Link>
-                <Link href={"/"}>About</Link>
+			<div className="lg:inline-flex gap-8 items-center max-[1025px]:hidden">
+				<Link href={"/#services"} className="inline-flex items-center gap-1">
+					Services
+				</Link>
+				<Link href={"/"}>Projects</Link>
+				<Link href={"/"}>About</Link>
 
-                <ChooseServiceDialog variant={scrollYPage > 500 ? "default" : "ghost"} />
-            </div>
-            <div className="hamburger-wrapper lg:hidden max-[1025px]:block">
-                <Sheet>
-                    <SheetTrigger><Menu/></SheetTrigger>
-                    <SheetContent side={"top"}>
-                        <SheetTitle>
-                            <Link href={"/"}>
-                                <Image width={250} height={250} className="object-fit w-6/12" src="logo.png"
-                                       alt="Webique Logo"/>
-                            </Link>
-                        </SheetTitle>
-                        <div className="flex flex-col gap-8 items-start justify-center p-6">
-                            <Link href={"/#services"} className="inline-flex items-center gap-1">
-                                Services
-                            </Link>
-                            <Link href={"/"}>Projects</Link>
-                            <Link href={"/"}>About</Link>
+				<ChooseServiceDialog variant={scrollYPage > 500 ? "default" : "ghost"}/>
+			</div>
+			<div className="hamburger-wrapper lg:hidden max-[1025px]:block">
+				<Sheet>
+					<SheetTrigger><Menu/></SheetTrigger>
+					<SheetContent side={"top"}>
+						<SheetTitle>
+							<Link href={"/"}>
+								<Image width={250} height={250} className="object-fit w-6/12" src="logo.png"
+											 alt="Webique Logo"/>
+							</Link>
+						</SheetTitle>
+						<div className="flex flex-col gap-8 items-start justify-center p-6">
+							<Link href={"/#services"} className="inline-flex items-center gap-1">
+								Services
+							</Link>
+							<Link href={"/"}>Projects</Link>
+							<Link href={"/"}>About</Link>
 
-                            <ScheduleACallButton variant={"default"}
-                                                 service={"any"}
-                                                 className={"w-full"}
-                                                 withIcon={true}></ScheduleACallButton>
-                        </div>
-                    </SheetContent>
-                </Sheet>
-            </div>
-        </nav>
-    );
+							<ChooseServiceDialog variant={"default"} className={"w-full"}/>
+						</div>
+					</SheetContent>
+				</Sheet>
+			</div>
+		</nav>
+	);
 }
